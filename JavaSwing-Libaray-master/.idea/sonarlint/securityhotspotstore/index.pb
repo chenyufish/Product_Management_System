647b288ac9cd2b30fbcbb3ff822522ca87cb80ea
fishman <cy13692810010@163.com>
2024-03-14 13:33:17 +0800

    以前的数据库作业

---
+++ JavaSwing-Libaray-master/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+q
+ALibrary/src/com/ksamar/library/controller/user/UserContoller.java,3\6\36110f0783818a15f8728a3b43ec4e3d165280cb
+b
+2Library/src/com/ksamar/library/views/UserView.java,e\6\e6fb8230e045440951915c12e3c1692efae23ab4
+j
+:Library/src/com/ksamar/library/views/book/AddBookView.java,b\1\b107cf7d478e1a5072f11a0008c8befe90425d59
+r
+BLibrary/src/com/ksamar/library/controller/book/BookController.java,f\c\fc34d0550b90eb14abd543d98dfed81373734072
+b
+2Library/src/com/ksamar/library/views/BookView.java,a\1\a107438d13f1e24e9258e9322dd6c12d23a32de3
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
---
+++ JavaSwing-Libaray-master/Library/src/com/ksamar/library/views/book/DeleteBookView.java
@@ -0,0 +1,68 @@
+package com.ksamar.library.views.book;
+
+import com.ksamar.library.controller.book.BookController;
+import com.ksamar.library.tools.color.Colour;
+import com.ksamar.library.tools.image.Images;
+import com.ksamar.library.views.message.Message;
+
+import java.util.Vector;
+
+/**
+ * 删除商品窗口
+ *
+
+ */
+public class DeleteBookView extends Message {
+
+    /**
+     * 删除商品窗口
+     */
+    private static final DeleteBookView INSTANCE = new DeleteBookView();
+
+    /**
+     * 删除商品窗口
+     */
+    private DeleteBookView() {
+        // 删除商品面板标题
+        titleText = "商品出库";
+        messageIconLabel.setIcon(Images.errorIcon);
+        messageTitleLabel.setText(titleText);
+
+        // 删除商品事件按钮
+        confirmButton.setBackground(Colour.CE54D52);
+        confirmButton.addActionListener(e -> deleteBook());
+    }
+
+    /**
+     * 设置信息
+     */
+    public void setValue(Vector vector) {
+        setVisible(true);
+        setLocationRelativeTo(null);
+        messageTextLabel.setText("确认出库 " + vector.get(2).toString() + " 该商品吗？");
+        confirmButton.setName(vector.get(0).toString());
+    }
+
+    /**
+     * 删除商品
+     */
+    private void deleteBook() {
+        dispose();
+        int id = Integer.parseInt(confirmButton.getName());
+        int result = BookController.deleteBook(id);
+        if (result == 0) {
+            Message.showMessage("出库失败", "错误", 0);
+        } else if (result == 1) {
+            Message.showMessage("出库成功", "信息", 1);
+        }
+    }
+
+    /**
+     * 获取删除商品窗口
+     *
+     * @return 删除商品窗口
+     */
+    public static DeleteBookView getInstance() {
+        return INSTANCE;
+    }
+}
